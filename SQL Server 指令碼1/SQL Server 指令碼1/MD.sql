8ec7a4eb24a16eb90340cc3c6652d3f14eb2de28
funfunsin <33145640+funfunsin@users.noreply.github.com>
2017-12-08 16:50:44 +0800

    1208

--- SQL Server 指令碼1/SQL Server 指令碼1/MD.sql
+++ SQL Server 指令碼1/SQL Server 指令碼1/MD.sql
@@ -1,6 +1,6 @@
 --假設菜單編號M0000001~M9999999
 --產生菜單編號的函數
-alter function fnGetMealNo()
+create function fnGetMealNo()
 	returns char(8)
 as
 begin
@@ -33,48 +33,43 @@ begin
 	return 'M'+@mealID
 end
 
-
-
-
---假設產品編號O000000001~O999999999
---產生產品編號的函數
-create function fnGetOrderNo()
-	returns char(8)
+--產生ChecktaxID函數
+create function fnChecktaxID
+	(@taxID char(8))
+	returns char(1)
 as
 begin
-	declare @orderID char(8), @number int
-	select top 1 @orderID=orderNo from orderList
-	order by orderNo desc
+	declare @a char(8)
+	select @a=taxID	from store
+	where taxID=@taxID
 
 	if @@ROWCOUNT=0
-		set @orderID='0000001'
-	else
-	begin
-		set @number=cast(substring(@orderID,2,8) as int)+1
-		set @orderID=cast(@number as varchar)
-		--001 as int =1 ->2
+		return '1'
 
-		if len(@orderID)=1
-			return 'O00000000'+@orderID
-		if len(@orderID)=2
-			return 'O0000000'+@orderID
-		if len(@orderID)=3
-			return 'O000000'+@orderID
-		if len(@orderID)=4
-			return 'O00000'+@orderID
-		if len(@orderID)=5
-			return 'O0000'+@orderID
-		if len(@orderID)=6
-			return 'O000'+@orderID
-		if len(@orderID)=7
-			return 'O00'+@orderID
-		if len(@orderID)=8
-			return 'O0'+@orderID
-		if len(@orderID)=9
-			return 'O'+@orderID
-	end
-	return 'O'+@orderID
+	return '0'
 end
 
 
+select orderContains.orderNo,orderTime,meal.mealName,orderContains.quantity,orderContains.finished 
+from orderContains 
+inner join meal on orderContains.mealNo=meal.mealNo 
+inner join orderList on orderContains.orderNo=orderList.orderNo
+where taxID='01010101'
+
+select *
+from orderList 
+inner join orderContains on orderList.orderNo=orderContains.orderNo 
+inner join meal on orderContains.mealNo=meal.mealNo 
+where taxID='01010101' and taken=1
+
+select distinct orderList.orderNo,orderList.customerID,orderList.orderTime,orderList.total,orderList.takeTime 
+from orderList 
+inner join orderContains on orderList.orderNo=orderContains.orderNo 
+inner join meal on orderContains.mealNo=meal.mealNo 
+where taxID='01010101' and taken=1
+
+select meal.mealName,orderList.orderTime from orderContains inner join meal on orderContains.mealNo=meal.mealNo 
+inner join orderList on orderContains.orderNo=orderList.orderNo where taxID='01010101'
+
+select mealName from meal where taxID = '01010101'
 
